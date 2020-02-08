0602648e546eb63767c07b9b0aa73235e137c38c
Fagner Lima <contato@fagnerlima.pro.br>
2020-02-08 09:18:00 -0300

    Substitui asserts do assertj pelo JUnit 5

--- src/test/java/br/pro/fagnerlima/exercicio1/application/ValoresTest.java
+++ src/test/java/br/pro/fagnerlima/exercicio1/application/ValoresTest.java
@@ -1,7 +1,6 @@
 package br.pro.fagnerlima.exercicio1.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,23 +16,23 @@ public class ValoresTest {
 
     @Test
     public void testIns() {
-        assertThat(valores.ins(1)).isTrue();
-        assertThat(valores.ins(10)).isTrue();
+        assertTrue(valores.ins(1));
+        assertTrue(valores.ins(10));
     }
     
     @Test
     public void testIns_whenNaoPositivo() {
-        assertThat(valores.ins(0)).isFalse();
-        assertThat(valores.ins(-1)).isFalse();
+        assertFalse(valores.ins(0));
+        assertFalse(valores.ins(-1));
     }
 
     @Test
     public void testIns_whenLimiteAtingido() {
         for (int i = 1; i <= 10; i++) {
-            assertThat(valores.ins(i)).isTrue();
+            assertTrue(valores.ins(i));
         }
 
-        assertThat(valores.ins(11)).isFalse();
+        assertFalse(valores.ins(11));
     }
 
     @Test
@@ -42,20 +41,20 @@ public class ValoresTest {
         valores.ins(15);
         valores.ins(20);
 
-        assertThat(valores.del(0)).isEqualTo(10);
-        assertThat(valores.del(2)).isEqualTo(20);
+        assertEquals(valores.del(0), 10);
+        assertEquals(valores.del(2), 20);
     }
 
     @Test
     public void testDel_whenSizeZero() {
-        assertThat(valores.del(0)).isEqualTo(-1);
+        assertEquals(valores.del(0), -1);
     }
 
     @Test
     public void testDel_whenNotFound() {
         valores.ins(10);
 
-        assertThat(valores.del(1)).isEqualTo(-1);
+        assertEquals(valores.del(1), -1);
     }
 
     @Test
@@ -71,7 +70,7 @@ public class ValoresTest {
         valores.ins(15);
         valores.ins(20);
 
-        assertThat(valores.size()).isEqualTo(3);
+        assertEquals(valores.size(), 3);
     }
 
     @Test
@@ -81,7 +80,7 @@ public class ValoresTest {
         valores.ins(20);
         valores.del(1);
 
-        assertThat(valores.size()).isEqualTo(2);
+        assertEquals(valores.size(), 2);
     }
 
 }

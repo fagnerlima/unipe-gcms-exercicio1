fef86cafd9b6300beb4efe53530c7778b0c2d47e
Fagner Lima <contato@fagnerlima.pro.br>
2020-02-08 10:24:04 -0300

    Adiciona exercícios do Ant

--- src/main/java/br/pro/fagnerlima/exercicio1/application/ValoresITF.java
+++ src/main/java/br/pro/fagnerlima/exercicio1/application/ValoresITF.java
@@ -2,27 +2,10 @@ package br.pro.fagnerlima.exercicio1.application;
 
 public interface ValoresITF {
 
-    /**
-     * Insere um valor
-     *
-     * @param v valor a ser inserido
-     * @return true se o valor for inserido
-     */
     boolean ins(int v);
 
-    /**
-     * Remove o valor armazenado no índice i e retorna o valor removido
-     *
-     * @param i índice do elemento
-     * @return valor removido
-     */
     int del(int i);
 
-    /**
-     * Retorna a quantidade de valores armazenados
-     *
-     * @return quantidade de valores
-     */
     int size();
 
 }
